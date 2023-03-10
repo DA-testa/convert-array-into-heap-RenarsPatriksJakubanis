bb26dfff5ed8fe5625d85f036d87a78be2a647e2
RenarsPatriksJakubanis <125654711+RenarsPatriksJakubanis@users.noreply.github.com>
2023-03-10 12:38:45 +0000

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,14 +1,13 @@
 # python3
 def build_heap(data):
     swaps = []
-    temp = len(data)
-    for i in range(temp // 1, -1, -1):
+    for i in range(len(data) // 2, -1, -1):
         while (i * 2) + 1 < len(data):
-            heap_ch = 1 + (2 * i)
-            if data[heap_ch] > data[i]:
-                break
-            elif len(data) > heap_ch + 1 and data[heap_ch] > data[heap_ch + 1]:
+            heap_ch = 2 * i + 1
+            if len(data) > heap_ch + 1 and data[heap_ch] > data[heap_ch + 1]:
                 heap_ch = heap_ch + 1
+            if data[heap_ch] >= data[i]:
+                break
             swaps.append((i, heap_ch))
             data[heap_ch], data[i] = data[i], data[heap_ch]
             i = heap_ch
