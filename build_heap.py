7423f6a977ad0fa9a17aa714f9fd9b039ee8803a
RenarsPatriksJakubanis <125654711+RenarsPatriksJakubanis@users.noreply.github.com>
2023-03-10 12:10:54 +0000

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -3,32 +3,34 @@ def build_heap(data):
     swaps = []
 
     for i in range(len(data) // 1, -1, -1):
-        while i * 2 + 1 < len(data):
-            heap_ch = 1 + 2 * i
-            if data[heap_ch + 1] < data[heap_ch] and heap_ch + 1 < len(data):
-                heap_ch = heap_ch + 1
-            if data[i] < data[heap_ch] or heap_ch > len(data):
+        while (i * 2) + 1 < len(data):
+            heap_ch = 1 + (2 * i)
+            if data[heap_ch] > data[i]:
                 break
-            data[heap_ch], data[i] = data[i], data[heap_ch]
+            elif len(data) > heap_ch + 1 and data[heap_ch] > data[heap_ch + 1]:
+                heap_ch = heap_ch + 1
             swaps.append((i, heap_ch))
+            data[heap_ch], data[i] = data[i], data[heap_ch]
             i = heap_ch
     return swaps
 
 def main():
-    swaps = []
     i = input()
     if i == "I":
-        n = input()
+        n = int(input())
         data = list(map(int, input().split()))
         swaps = build_heap(data)
+        assert len(data) == n
+        
     elif i == "F":
-        n = input()
-        location  = "tests/" + n
+        input_key = input()
+        location  = "tests/" + input_key
         with open(location) as f:
-            n = f.read()
-            data = list(map(int, n.split()))
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
             swaps = build_heap(data)
-   
+            assert len(data) == n
+    
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
